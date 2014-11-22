2933fab820e03957eda069639304578dd4130c1f
David <davidlavoy@me.com>
2014-11-22 16:40:55 +0000

    Added Path Var to Minecraft install

--- minecraft.py
+++ minecraft.py
@@ -11,36 +11,38 @@ while ans:
         print("\ntekkit : will install Offical Latest Tekkit Minecraft")
         print("\n=====")
     elif ans == "install 1.8":
-        server = "/root/"+raw_input("Server Name: ")
-        if os.path.exists(server):
+        server = raw_input("Server Name: ")
+        path ="/root/minecraft/"
+        if os.path.exists(path+server):
             print("Server "+server+" Already Exist")
             pass
         else:
-            os.mkdir(server) 
+            print("1")
+            os.makedirs(path+server);
             ram = raw_input("Ram: ")
             print("\n=====Opening Minecraft Port=====")
             subprocess.call("ufw allow 25565",shell=True)
-            print("\n=====Creating Directory "+server+"=====")
+            print("\n=====Creating Directory "+path+server+"=====")
             print("\n=====Downloading Offical Latest Minecraft 1.8=====")
             subprocess.call("wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar",shell=True)
             print("\n=====Creating Startup Script=====")
-            subprocess.call("echo 'cd "+server+";java ""-Xmx"+ram+"M -Xms"+ram+"M -jar minecraft_server.jar -nogui' > "+server+"/start.sh",shell=True)
+            subprocess.call("echo 'cd "+path+server+";java ""-Xmx"+ram+"M -Xms"+ram+"M -jar minecraft_server.jar -nogui' > "+path+server+"/start.sh",shell=True)
             subprocess.call("echo eula=true > eula.txt",shell=True)
-            print("\n=====Moving files to "+server+"=====")
-            subprocess.call("mv minecraft_server.1.8.jar "+server+"/minecraft_server.jar",shell=True)
-            subprocess.call("mv eula.txt "+server,shell=True)
+            print("\n=====Moving files to "+path+server+"=====")
+            subprocess.call("mv minecraft_server.1.8.jar "+path+server+"/minecraft_server.jar",shell=True)
+            subprocess.call("mv eula.txt "+path+server,shell=True)
             print("\n=====Setting Permissons=====")
             print("\n=====Before=====")
-            subprocess.call("ls -la "+server,shell=True)
-            subprocess.call("chmod 755 "+server+"/start.sh",shell=True)
+            subprocess.call("ls -la "+path+server,shell=True)
+            subprocess.call("chmod 755 "+path+server+"/start.sh",shell=True)
             print("\n=====After=====")
-            subprocess.call("ls -la "+server,shell=True)
+            subprocess.call("ls -la "+path+server,shell=True)
     elif ans == "install tekkit":
         print("need to config the server ")
     elif ans == "start server":
       server = raw_input("Server Name: ")
       print("\nStarting")
-      subprocess.Popen("sh /root/"+server+"/start.sh",shell=True)
+      subprocess.Popen("sh "+path+server+"/start.sh",shell=True)
     elif ans == "stop server":
       print("\n Server is stopping")
     elif ans == "restart server":
