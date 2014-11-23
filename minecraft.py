53e3e11ad6eddfe8266d91fdf2f9a47c3d015529
David <davidlavoy@me.com>
2014-11-23 01:57:17 +0000

    updated minecraft installer script

--- minecraft.py
+++ minecraft.py
@@ -18,32 +18,34 @@ while ans:
     elif ans == "install 1.8":
         minecraft_version = "Offical Minecraft 1.8"
         minecraft_download = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar'        
-    elif ans == "install tekkit"
+        break 
+    elif ans == "install tekkit":
         minecraft_version = "Offical tekkit"
         minecraft_download = "tekkit link"
-    else 
-        print("\n Not A vaild Selection")
+        break
+    else: 
+        print("\n Not A valid Selection")
 
-        server = raw_input("Server Name: ")
-        if os.path.exists(path+server):
-            print("Server "+server+" Already Exist")
-            pass
-        else:
-            os.makedirs(path+server);
-            ram = raw_input("Ram: ")
-            print("\n=====Opening Minecraft Port=====")
-            subprocess.call("ufw allow 25565",shell=True)
-            print("\n=====Creating Directory "+path+server+"=====")
-            print("\n=====Downloading "+minecraft_version+"=====")
-            subprocess.call("wget "+minecraft_download,shell=True)
-            print("\n=====Creating Startup Script=====")
-            subprocess.call("echo 'cd "+path+server+";java ""-Xmx"+ram+"M -Xms"+ram+"M -jar minecraft_server.jar -nogui' > "+path+server+"/start.sh",shell=True)
-            subprocess.call("echo eula=true > eula.txt",shell=True)
-            print("\n=====Moving files to "+path+server+"=====")
-            subprocess.call("mv minecraft_server.1.8.jar "+path+server+"/minecraft_server.jar",shell=True)
-            subprocess.call("mv eula.txt "+path+server,shell=True)
-            print("\n=====Setting Permissons=====")
-            print("\n=====Before=====")
-            subprocess.call("ls -la "+path+server,shell=True)
-            subprocess.call("chmod 755 "+path+server+"/start.sh",shell=True)
-            print("\n=====After=====")
+server = raw_input("Server Name: ")
+if os.path.exists(path+server):
+    print("Server "+server+" Already Exist")
+    pass
+else:
+    os.makedirs(path+server);
+    ram = raw_input("Ram: ")
+    print("\n=====Opening Minecraft Port=====")
+    subprocess.call("ufw allow 25565",shell=True)
+    print("\n=====Creating Directory "+path+server+"=====")
+    print("\n=====Downloading "+minecraft_version+"=====")
+    subprocess.call("wget "+minecraft_download,shell=True)
+    print("\n=====Creating Startup Script=====")
+    subprocess.call("echo 'cd "+path+server+";java ""-Xmx"+ram+"M -Xms"+ram+"M -jar minecraft_server.jar -nogui' > "+path+server+"/start.sh",shell=True)
+    subprocess.call("echo eula=true > eula.txt",shell=True)
+    print("\n=====Moving files to "+path+server+"=====")
+    subprocess.call("mv minecraft_server.1.8.jar "+path+server+"/minecraft_server.jar",shell=True)
+    subprocess.call("mv eula.txt "+path+server,shell=True)
+    print("\n=====Setting Permissons=====")
+    print("\n=====Before=====")
+    subprocess.call("ls -la "+path+server,shell=True)
+    subprocess.call("chmod 755 "+path+server+"/start.sh",shell=True)
+    print("\n=====After=====")
