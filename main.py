78a907bf4e11c36b20cc79d4f7449f649bdf5e77
David <davidlavoy@me.com>
2014-11-28 02:26:06 +0000

    updated print to a functions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import shutil
 import sys
 import config
+from termcolor import colored
 
 # Help location
 helplocation = config.h['main']
@@ -9,17 +10,15 @@ version = config.rv['version']
 
 subprocess.call("figlet Pygate {}".format(version),shell=True)
 ans=True
-print("PYGate Main Menu")
+config.output_r("PYGate Main Menu")
 while ans:
     ans = raw_input("[PyGate]~> ")
     if ans == "minecraft":
         config.output_y ("Loading Minecraft Settings")
-        print("\n+-----| Loading Minecraft Settings |-----+")
         import minecraft
 
     elif ans == "starmade":
-        config.ouput_r("Loading Starmade Settings")
-        print("\n+-----| Loading Starmade Settings |----+")
+        config.output_y("Loading Starmade Settings")
         import starmade
 
     elif ans == "ss":
@@ -27,16 +26,16 @@ while ans:
 
     elif ans == "ping":
         p = raw_input("\nWhat will you like to ping: ")
-        print("\n +------| Starting |-----+")
+        config.output_g("Starting Ping")
         subprocess.call("ping -c 5 {}".format(p),shell=True)
-        print("\n +-----| Complete |-----+")
-    
+        config.output_g("Complete")
+         
     elif ans == "clear":
         subprocess.call("clear")
     
     elif ans == "git":
     
-        config.output_b("Checking for updates===")
+        config.output_b("Checking for updates")
         subprocess.call("git pull",shell=True)
         config.output_b("Pushing up Github" )
         commit = raw_input("Commit: ")
@@ -44,7 +43,7 @@ while ans:
         subprocess.call("git add .",shell=True)
         subprocess.call("git commit -m "+"'"+commit+"'",shell=True)
         subprocess.call("git push",shell=True)
-        print("\n ===Complete===")
+        config.output_b("Complete")
     
     elif ans == "exit":
         print("\n Goodbye")
