f80bed99e6e87ec00ad3003703e8fa0cdeeca057
David ubuntu1 <davidlavoy@me.com>
2015-01-27 01:45:27 +0000

    Made Mc_function more readable

--- minecraft/mc_function.py
+++ minecraft/mc_function.py
@@ -50,7 +50,7 @@ def installer():
 
     else:
         ram = raw_input("Ram in MB: ")
-        c = path['minecraft_path']+server
+        path = path['minecraft_path']+server
         os.makedirs(c)
 
         #check if ufw is enabled
@@ -64,33 +64,31 @@ def installer():
         elif "Status: inactive" in status:
             lob.output_y("Ufw is inactive Skipping")
         else:
-            lob.output_y("Opening Minecraft Port")
-
-            subprocess.call("ufw allow 25565", shell=True)
+            lob.output_y("There was a error checking ufw")
 
         lob.output_b("Creating Directory")
-        lob.output_b("Created Directory {}".format(c))
+        lob.output_b("Created Directory {}".format(path))
 
         lob.output_b("Downloading {}".format(m_v))
         # Need to add logic to check for pre downloaded minecraft jar
         subprocess.call("wget {}".format(m_d), shell=True)
 
         lob.output_b("Creating Startup Script")
-        subprocess.call("echo 'java -Xmx{}M -Xms{}M -jar minecraft_server.jar -nogui' > {}/start.sh".format(ram, ram, c), shell=True)
+        subprocess.call("echo 'java -Xmx{}M -Xms{}M -jar minecraft_server.jar -nogui' > {}/start.sh".format(ram, ram, path), shell=True)
         subprocess.call("echo eula=true > eula.txt", shell=True)
 
-        lob.output_b("Moving files to {}".format(c))
-        subprocess.call("mv minecraft_server.1.8.jar {}/minecraft_server.jar".format(c), shell=True)
-        subprocess.call("mv eula.txt {}".format(c), shell=True)
+        lob.output_b("Moving files to {}".format(path))
+        subprocess.call("mv minecraft_server.1.8.jar {}/minecraft_server.jar".format(path), shell=True)
+        subprocess.call("mv eula.txt {}".format(path), shell=True)
 
-        lob.output_b("Setting Permissons for {}".format(c))
+        lob.output_b("Setting Permissons for {}".format(path))
 
         lob.output_b("Before")
-        subprocess.call("ls -la {}".format(c), shell=True)
-        subprocess.call("chmod 755 {}/start.sh".format(c), shell=True)
+        subprocess.call("ls -la {}".format(path), shell=True)
+        subprocess.call("chmod 755 {}/start.sh".format(path), shell=True)
 
         lob.output_b("After")
-        subprocess.call("ls -la {}".format(c), shell=True)
+        subprocess.call("ls -la {}".format(path), shell=True)
 
         lob.output_b("Complete")
-        lob.output_b("The server is now installed in {}".format(c))
+        lob.output_b("The server is now installed in {}".format(path))
